55bca26ce4bb7558c7385abec370ec4ae4d6049c
Mr. Pancake - René <renehammink@icloud.com>
2025-01-14 17:49:46 +0100

    Player movement
    doesnt have new input system yet

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Drag;
+    public float MovementSpeed;
+    public float JumpForce;
+    private float HVelocity;
+    private float VVelocity;
+    private Rigidbody rb;
+    private float groundCheckDistance = 0.2f;
+    private LayerMask groundLayer;
+    public Vector3 boxSize;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        groundLayer = LayerMask.GetMask("Ground");
+    }
+    void FixedUpdate()
+    {
+        bool grounded = IsGrounded();
+        Debug.Log(grounded);
+
+        //jumping
+        if (grounded && Input.GetAxisRaw("Jump") == 1)
+        { 
+            rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+        }
+
+        //movement
+        float HAcceleration = (Input.GetAxisRaw("Horizontal") * MovementSpeed) - (Drag * HVelocity);
+        float VAcceleration = (Input.GetAxisRaw("Vertical") * MovementSpeed) - (Drag * VVelocity);
+        HVelocity += HAcceleration;
+        VVelocity += VAcceleration;
+        rb.velocity = new Vector3(HVelocity , rb.velocity.y, VVelocity);
+    }
+
+    public bool IsGrounded()
+    {
+        // Cast a ray downward from the player's position
+        return Physics.OverlapBox(transform.position + (Vector3.down * transform.localScale.y), boxSize / 2, Quaternion.identity, groundLayer).Length > 0;
+    }
+
+    public void OnDrawGizmos()
+    {
+        // Draw the ray in the Scene view for debugging
+        Gizmos.color = Color.green;
+        Gizmos.DrawCube(transform.position + Vector3.down * transform.localScale.y, boxSize);
+    }
+}
